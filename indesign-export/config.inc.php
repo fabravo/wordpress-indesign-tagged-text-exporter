451438e61cbac0a0cbdacb7493748fdd41816da9
Frank Bravo <fabravo@bravoitc.com>
2024-05-13 11:52:58 -0700

    Removed post status array

--- indesign-export/config.inc.php
+++ indesign-export/config.inc.php
@@ -6,7 +6,7 @@
 // Settings
 define('POSTS_PER_PAGE', 50); // -1 to include all posts
 define('WORDPRESS_ROLE_LEVEL', "read"); // 'publish_posts' for 'author' role and above, 'read' is for anyone who can at least read posts
-define('WORDPRESS_POST_STATUS', array('publish', 'draft')); // Include both published and draft posts
+define('WORDPRESS_POST_STATUS', ''); // either use one status or array() - https://wordpress.org/documentation/article/post-status/
 define('WORDPRESS_CATEGORIES_INCLUDED', ''); // list of categories ('real estate, print ready') or blank for all 
 define('WORDPRESS_CATEGORIES_EXCLUDE_UNCATEGORIZED', '1'); // values are 0 (false, or include them) or 1 (true, or exclude them) 
 define('WORDPRESS_TAGS_INCLUDED', 'print-ready'); // list of tags ('tag1, tag2') or blank for all
